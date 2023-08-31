4028edc7800bdb1d35eb7927697fcd8523cd508f
clicoverse <mywebchamp@gmail.com>
2023-09-01 00:18:35 +0530

    video card cread completed

--- script/script.js
+++ script/script.js
@@ -37,6 +37,41 @@ const categoryHandleClick = async (id) => {
 
 const displayVideos = (videos) => {
   console.log(videos);
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  videos.forEach((video) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card w-full bg-base-100 shadow-xl">
+          <figure>
+            <img class="w-full h-44" src=${video.thumbnail} alt="Shoes" />
+          </figure>
+          <div>
+            <div class="card-body flex justify-between">
+              <div class="flex space-x-5">
+                <div class="avatar">
+                  <div class="w-14 rounded-full">
+                    <img
+                      src=${video.authors[0].profile_picture} />
+                  </div>
+                </div>
+                <h2 class="card-title">
+                  ${video.title}</h2>
+              </div>
+              <div class="px-20">
+                <div class="flex gap-2">
+                  <h6>${video.authors[0].profile_name}</h6>
+                  <img class="hidden" src="./img/fi_10629607.svg" alt="varified">
+                </div>
+                <small>${video.others.views}</small>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+    `;
+    cardContainer.appendChild(div);
+  });
 };
 
 loadHandlerCategory();
